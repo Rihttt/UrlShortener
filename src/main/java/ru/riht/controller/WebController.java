617b880d9689e7eaf4d146c3bcf1f005676f27cd
Rihttt <riht1125@gmail.com>
2025-09-08 17:29:27 +0600

    Added user interface and option for custom link

--- src/main/java/ru/riht/controller/WebController.java
+++ src/main/java/ru/riht/controller/WebController.java
@@ -1,11 +1,16 @@
 package ru.riht.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
+import ru.riht.model.Link;
 import ru.riht.service.LinkService;
 
 @Controller
@@ -15,6 +20,31 @@ public class WebController {
 
     public WebController(LinkService linkService) {
         this.linkService = linkService;
+    }
+
+    @PostMapping("/shorten")
+    public String shorten(@RequestParam String originalUrl,
+                          @RequestParam String customCode,
+                          Model model) {
+        String shortUrl ="";
+
+        try {
+            if(customCode != null) {
+                linkService.shortenLink(originalUrl, customCode);
+                shortUrl = "http://localhost:8080/" + customCode;
+            }else{
+                Link shortenedLink = linkService.shortenLink(originalUrl, customCode);
+                shortUrl = "http://localhost:8080/" + shortenedLink.getShortCode();
+            }
+        } catch (DataIntegrityViolationException error) {
+            DataIntegrityViolationException e = new DataIntegrityViolationException("Такой код уже существует: " + customCode);
+            model.addAttribute("error", e.getMessage());
+        }
+
+
+        model.addAttribute("originalUrl", originalUrl);
+        model.addAttribute("shortUrl", shortUrl);
+        return "index";
     }
 
     @GetMapping("/{shortCode}")
