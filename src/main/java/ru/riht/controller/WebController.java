0f662772c495009fb55ffe9d0404b175e84ba907
Rihttt <riht1125@gmail.com>
2025-09-21 13:49:10 +0600

    Adjusted file structure, upgraded history functional with redirect and copy buttons, added placeholder button for qr code

--- src/main/java/ru/riht/controller/WebController.java
+++ src/main/java/ru/riht/controller/WebController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import ru.riht.model.Link;
@@ -18,6 +19,8 @@ import ru.riht.service.LinkService;
 import ru.riht.model.projections.LinkDto;
 
 import java.util.List;
+
+import static ru.riht.constants.ServiceConstants.SHORT_URL;
 
 @Controller
 public class WebController {
@@ -46,20 +49,22 @@ public class WebController {
                           @RequestParam String customCode,
                           HttpServletRequest request,
                           HttpServletResponse response,
+                          RedirectAttributes redirectAttributes,
                           Model model) {
 
         String userId = linkService.getUserIdFromCookie(request, response);
-
 
         String shortUrl ="";
 
         try {
             if(!customCode.isEmpty()) {
                 linkService.shortenLink(originalUrl, customCode, userId);
-                shortUrl = "http://localhost:8080/" + customCode;
+                shortUrl = SHORT_URL + customCode;
+                redirectAttributes.addFlashAttribute("shortUrl", shortUrl);
             }else{
                 Link shortenedLink = linkService.shortenLink(originalUrl, customCode, userId);
-                shortUrl = "http://localhost:8080/" + shortenedLink.getShortCode();
+                shortUrl = SHORT_URL + shortenedLink.getShortCode();
+                redirectAttributes.addFlashAttribute("shortUrl", shortUrl);
             }
         } catch (DataIntegrityViolationException error) {
             DataIntegrityViolationException e = new DataIntegrityViolationException("Такой код уже существует: " + customCode);
@@ -71,7 +76,7 @@ public class WebController {
         model.addAttribute("userLinks", userLinks);
         model.addAttribute("originalUrl", originalUrl);
         model.addAttribute("shortUrl", shortUrl);
-        return "index";
+        return "redirect:/";
     }
 
     @GetMapping("/{shortCode}")
